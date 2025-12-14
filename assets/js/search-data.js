637a4b14c9c99ea7d5712b2e65bd462ae35bccb1
drossat <donrossat@gmail.com>
2025-12-14 13:03:30 +0000

    Deploying to gh-pages from @ drossat/drossat.github.io@f19f0a787aae3eec49773c63843696b4e348030f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,7 +45,10 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "news-internship-offer-physics-informed-bayesian-inference-for-computational-mechanics",
+      },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-internship-offer-physics-informed-bayesian-inference-for-computational-mechanics",
           title: 'Internship offer - Physics-Informed Bayesian Inference for Computational Mechanics',
           description: "",
           section: "News",handler: () => {
